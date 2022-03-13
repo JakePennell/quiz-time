475fd6a00662a9d11dc336a80a87689c461d3ceb
JakePennell <jake.pennell3@gmail.com>
2022-03-13 11:12:02 +0000

    add first question and answer choices

--- assets/js/game.js
+++ assets/js/game.js
@@ -3,3 +3,18 @@ const choices = Array.from(document.querySelectorAll('.choice-text'));
 const progressText = document.querySelector('#progressText');
 const scoreText = document.querySelector('#score');
 const progressBarFull = document.querySelector('#progressBarFull');
+
+let currentQuestion = {};
+let acceptingAnswers = true;
+let score = 0;
+let questionCounter = 0;
+let availableQuestions = []
+
+let questions = [{
+    question: 'What is 2 + 2',
+    Choice1: '2',
+    Choice2: '4',
+    Choice3: '21',
+    Choice4: '17',
+    answer: 2,
+}]
