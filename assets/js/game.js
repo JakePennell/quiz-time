72d6c56df7817513ff5f27d94d35dedc88603ec3
JakePennell <jake.pennell3@gmail.com>
2022-03-13 11:18:55 +0000

    add 3 additional question and answers

--- assets/js/game.js
+++ assets/js/game.js
@@ -11,10 +11,35 @@ let questionCounter = 0;
 let availableQuestions = []
 
 let questions = [{
-    question: 'What is 2 + 2',
-    Choice1: '2',
-    Choice2: '4',
-    Choice3: '21',
-    Choice4: '17',
-    answer: 2,
-}]
+        question: 'What is 2 + 2?',
+        Choice1: '2',
+        Choice2: '4',
+        Choice3: '21',
+        Choice4: '17',
+        answer: 2,
+    },
+    {
+        question: 'What the capital of England?',
+        Choice1: 'London',
+        Choice2: 'Paris',
+        Choice3: 'Madrid',
+        Choice4: 'Rome',
+        answer: 1,
+    },
+    {
+        question: 'What team does Harry Kane play for?',
+        Choice1: 'Man Utd',
+        Choice2: 'Man City',
+        Choice3: 'Spurs',
+        Choice4: 'Chelsea',
+        answer: 3,
+    },
+    {
+        question: 'What is the hottest planet?',
+        Choice1: 'Earth',
+        Choice2: 'Mars',
+        Choice3: 'Saturn',
+        Choice4: 'Venus',
+        answer: 4,
+    }
+]
