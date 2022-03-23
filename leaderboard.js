38c84b9783cdff4c2931b3e4228937e418f2608e
JakePennell <jake.pennell3@gmail.com>
2022-03-23 15:43:27 +0000

    tidy up leaderboard.js after running JShint validator

--- leaderboard.js
+++ leaderboard.js
@@ -5,5 +5,5 @@ const highScores = JSON.parse(localStorage.getItem('highScore')) || [];
 //Display high scores and which user achieved them.//
 highScoresList.innerHTML =
     highScores.map(score => {
-        return `<li class="high-score">${score.name} - ${score.score}</li>`
+        return `<li class="high-score">${score.name} - ${score.score}</li>`;
     }).join('');
